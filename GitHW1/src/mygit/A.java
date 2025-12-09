dd0d1afab1716aa8448c19532e581239800b0ffd
scr2y <scr2y@LAPTOP-IBK98JKG>
2025-12-09 22:17:01 +0900

    Bob branch에서 A.java, B.java 수정

--- GitHW1/src/mygit/A.java
+++ GitHW1/src/mygit/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("git에 초기버전 생성");
 		System.out.println("A is an apple");
+		System.out.println("I made a Bob branch.");
 	}
 
 }
--- GitHW1/src/mygit/B.java
+++ GitHW1/src/mygit/B.java
@@ -4,6 +4,7 @@ public class B {
 
 	public void sayGit(String toYou) {
 		System.out.println("git에 생성" + toYou);
+		System.out.println("B is a banana.");
 
 	}
 
