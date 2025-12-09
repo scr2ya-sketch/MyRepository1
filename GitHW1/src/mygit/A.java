b1ef5b094268c1ee739670cbf9b564fb203d0dd5
scr2y <scr2y@LAPTOP-IBK98JKG>
2025-12-09 22:10:29 +0900

    Alice branch에서 A.java, C.java 수정

--- GitHW1/src/mygit/A.java
+++ GitHW1/src/mygit/A.java
@@ -5,6 +5,9 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("git에 초기버전 생성");
 		System.out.println("A is an apple");
+		C apple = new C();
+		apple.sayGit("Apple");
+		
 	}
 
 }
--- GitHW1/src/mygit/C.java
+++ GitHW1/src/mygit/C.java
@@ -2,8 +2,8 @@ package mygit;
 
 public class C {
 
-	public static void main(String[] args) {
-		System.out.println("git에 초기버전 생성");
+	public void sayGit(String toYou) {
+		System.out.println("Cat" + toYou);
 
 	}
 
